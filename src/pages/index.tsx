7a3e437df0b7e460cd07fdd959a95ecb1d425e78
Murilo Cunha <murilo.sant@hotmail.com>
2021-03-27 01:32:05 -0300

    feat: added global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import styles from "styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
       Hello <span>styles</span>
-    </h1>
+    </>
   );
 }
