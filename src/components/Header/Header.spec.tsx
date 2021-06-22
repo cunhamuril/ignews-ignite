a4949990505edfd5d0268780f97c406f4ac72511
Murilo Cunha <murilo.sant@hotmail.com>
2021-06-22 11:11:05 -0300

    test: header

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react";
+import Header from ".";
+
+// Imitando next/router
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+// jest.mock("next-auth/client", () => {
+//   return {
+//     useSession() {
+//       return [null, false];
+//     },
+//   };
+// });
+
+describe("Header component", () => {
+  it("renders correctly", () => {
+    const { getByText } = render(<Header />);
+
+    expect(getByText("Home")).toBeInTheDocument();
+    expect(getByText("Posts")).toBeInTheDocument();
+  });
+});
