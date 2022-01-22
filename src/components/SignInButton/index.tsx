9df837d1dadf5d64c1f3168958ddd8381c7e55d2
Murilo Cunha <murilo.sant@hotmail.com>
2022-01-22 13:54:34 -0300

    adicionando FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,18 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn, useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 const SignInButton: React.FC = () => {
   const [session] = useSession();
 
-  console.log(session);
-
   return session ? (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
       {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
